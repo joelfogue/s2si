c9f4f153889fee3faf774ae1f36051333fdd3ddc
Joel Fogue <joel.fogue@quickpivot.com>
2020-01-18 22:25:52 -0500

    Adding CreateNewBusiness POST endpoint

---
+++ api/createNewBusiness/app.py
@@ -0,0 +1,46 @@
+import json
+import logging
+import os
+import uuid
+import boto3
+from botocore.config import Config
+
+log_level = os.environ.get('LOG_LEVEL', 'INFO')
+logging.root.setLevel(logging.getLevelName(log_level))
+_logger = logging.getLogger(__name__)
+
+# This is actually the default per:
+# https://github.com/boto/botocore/blob/15ecfbc7ea23f81981ca65626ee166df130f64db/botocore/data/_retry.json#L119-L126
+AWS_CONFIG = Config(retries={'max_attempts': 10})
+
+BUSINESS_TABLE_NAME = os.environ.get('BUSINESS_TABLE_NAME')
+dynamodb = boto3.resource('dynamodb', config=AWS_CONFIG)
+dynamodb = dynamodb.Table(BUSINESS_TABLE_NAME)
+#uuid=uuid.uuid4(uuid.NAMESPACE_DNS, 'python.org')
+
+
+def insert_item_into_db(item):
+    print("------------------PUT ITEM-----------------")
+    dynamodb.put_item(
+        TableName=BUSINESS_TABLE_NAME,
+        Item=item
+    )
+
+
+def lambda_handler(event, context):
+    _logger.debug('Event received: {}'.format(json.dumps(event)))
+    print("-------------------EVENT-----------------------")
+    print(event)
+    item = json.loads(event.get('body'))
+    item["BusinessId"] = str(uuid.uuid4())
+
+    insert_item_into_db(item)
+    response = {
+        'statusCode': 200,
+        'body': json.dumps(
+            {'success': True,
+             "message": item}
+        )
+    }
+    _logger.debug('responseonse: {}'.format(json.dumps(response)))
+    return response

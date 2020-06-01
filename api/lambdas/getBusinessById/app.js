9f58ce21b4515ca7a911b9251d82441e837e32ca
Joel Fogue <joel.fogue@quickpivot.com>
2020-05-31 21:11:55 -0400

    Get business by id

--- api/lambdas/getBusinessById/app.js
+++ api/lambdas/getBusinessById/app.js
@@ -1,20 +1,40 @@
-
 let response;
+const dynamodb = require('aws-sdk/clients/dynamodb');
+const docClient = new dynamodb.DocumentClient();
 
 exports.lambdaHandler = async (event, context) => {
+    const businessTable = process.env.BUSINESS_TABLE_NAME;
     try {
-        // const ret = await axios(url);
-        response = {
-            'statusCode': 200,
-            'body': JSON.stringify({
-                message: 'This lambda will getBusinessById',
-                // location: ret.data.trim()
-            })
+        if (event.httpMethod !== 'GET') {
+            throw new Error(`getBusinessById only accept GET method, you tried: ${event.httpMethod}`);
         }
+        // Writing logs to CloudWatch
+        console.info('received:', event);
+        const businessId = event.pathParameters.BusinessId;
+
+        var params = {
+            TableName:businessTable,
+            Key:{
+                "BusinessId": businessId
+            }
+        };
+        const data = await docClient.get(params).promise();
+        console.log('**************************');
+        console.log(data);
+
+        const item = data.Item;
+
+        const response = {
+            statusCode: 200,
+            body: JSON.stringify(item)
+        };
+
+        // Writing logs to CloudWatch
+        console.info(`response from: ${event.path} statusCode: ${response.statusCode} body: ${response.body}`);
+        return response;
+
     } catch (err) {
         console.log(err);
         return err;
     }
-
-    return response
 };
--- api/lambdas/updateExistingBusiness/app.js
+++ api/lambdas/updateExistingBusiness/app.js
@@ -1,20 +1,42 @@
-
 let response;
+const dynamodb = require('aws-sdk/clients/dynamodb');
+const docClient = new dynamodb.DocumentClient();
 
-exports.lambdaHandler = async (event, context) => {
+exports.lambdaHandler = async (event) => {
     try {
-        // const ret = await axios(url);
-        response = {
-            'statusCode': 200,
-            'body': JSON.stringify({
-                message: 'This lambda will updateExistingBusiness',
-                // location: ret.data.trim()
-            })
+        const businessTable = process.env.BUSINESS_TABLE_NAME;
+
+        if (event.httpMethod !== 'PUT') {
+            throw new Error(`updateExistingBusiness only accepts PUT method, you tried: ${event.httpMethod} method.`);
         }
+        // Writing logs to CloudWatch
+        console.info('received:', event);
+
+        // Get id and name from the body of the request
+        const body = JSON.parse(event.body)
+        const businessId = event.pathParameters.BusinessId;
+        const name = body.name;
+
+        var params = {
+            TableName: tableName,
+            Item: {
+                id: id,
+                name: name
+            }
+        };
+
+        const result = await docClient.put(params).promise();
+
+        const response = {
+            statusCode: 200,
+            body: JSON.stringify(body)
+        };
+
+        // Writing logs to CloudWatch
+        console.info(`response from: ${event.path} statusCode: ${response.statusCode} body: ${response.body}`);
+        return response;
     } catch (err) {
         console.log(err);
         return err;
     }
-
-    return response
-};
+}
